904015b5c92ab018c30ba1b50df7e2aeb7602b8b
Ubuntu <ubuntu@makeit.cat>
2020-08-11 01:12:06 +0200

    send endpoint

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var sendinfo;
 
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/send/:info', (req,res) => {
+  res.sendFile(__dirname + '/void.html');
+  io.emit('chat message', req.params.info);
 });
 
 io.on('connection', (socket) => {
